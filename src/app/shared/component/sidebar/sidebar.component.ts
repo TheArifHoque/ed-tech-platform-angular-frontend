9d3a7d6c96c1115ea43f29cdb9b018d6e0b6c35e
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-18 19:47:32 +0600

    login logout functionality added

--- src/app/shared/component/sidebar/sidebar.component.ts
+++ src/app/shared/component/sidebar/sidebar.component.ts
@@ -1,18 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../service/auth.service';
+import { MenuItem } from '../../model/menu-item';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   isLoggedIn: boolean;
   showSidebar: boolean;
+  isAdmin: boolean;
+
+  userMenu: MenuItem[] = [
+    { label: 'Home', route: '/' },
+    { label: 'Learning Dashboard', route: '/' },
+    { label: 'Notification', route: '/' },
+    { label: 'Manage Profile', route: '/' },
+  ];
+  adminMenu: MenuItem[] = [
+    { label: 'Home', route: '/' },
+    { label: 'Dashboard', route: '/admin/dashboard' },
+    { label: 'Profile', route: '/admin/general/profile' },
+    { label: 'Notification', route: '/admin/notification' },
+    { label: 'Payment Request', route: '/admin/payments' },
+    { label: 'Statistics', route: '/admin/statistics' },
+  ]
 
   constructor(private authService: AuthService) {
     this.isLoggedIn = false;
     this.showSidebar = false;
+    this.isAdmin = false;
+  }
+
+  ngOnInit(): void {
+    this.setLoginStatusAndLoadUserInfo();
+  }
+
+  setLoginStatusAndLoadUserInfo(): void {
+    this.authService.isLoggedIn().subscribe((loggedInStatus) => {
+      this.isLoggedIn = loggedInStatus;
+      if (this.isLoggedIn) {
+        this.isAdmin = this.authService.isAdmin();
+        // this.loadProfileData();
+      }
+    })
+  }
+
+  getUserName(): string {
+    return this.authService.getUsername();
+  }
+
+  getActiveMenu(): MenuItem[] {
+    if (this.isLoggedIn && this.isAdmin) {
+      return this.adminMenu;
+    } else if (this.isLoggedIn) {
+      return this.userMenu;
+    }
+    return [];
   }
 
   toggleSidebar(): void {
@@ -21,5 +66,9 @@ export class SidebarComponent {
 
   login(): void {
     this.authService.login();
+  }
+
+  logout(): void {
+    this.authService.logout();
   }
 }
