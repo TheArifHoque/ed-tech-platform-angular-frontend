964d335831cc34c8d719bf9962c1bd62d61a0613
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-22 12:21:38 +0600

    home-page component initiated and routing provided

--- src/app/general/general-routing.module.ts
+++ src/app/general/general-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CallbackPageComponent } from "./page/callback-page/callback-page.component";
+import { HomePageComponent } from "./page/home-page/home-page.component";
 
 const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        component: HomePageComponent,
+    },
     {
         path: 'callback',
         component: CallbackPageComponent,
--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CallbackPageComponent } from './page/callback-page/callback-page.component';
 import { GeneralRoutingModule } from './general-routing.module';
+import { HomePageComponent } from './page/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    CallbackPageComponent
+    CallbackPageComponent,
+    HomePageComponent
   ],
   imports: [
     CommonModule,
