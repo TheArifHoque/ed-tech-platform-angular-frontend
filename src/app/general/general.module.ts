67cb9461d3d2355816f8d4547871ac1de0eb0425
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-05-26 12:46:08 +0600

    profile-page component initiated

--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { CallbackPageComponent } from './page/callback-page/callback-page.component';
 import { GeneralRoutingModule } from './general-routing.module';
 import { HomePageComponent } from './page/home-page/home-page.component';
+import { ProfilePageComponent } from './page/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
     CallbackPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    ProfilePageComponent
   ],
   imports: [
     CommonModule,
