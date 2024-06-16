3f83d1cd4a9f631ac3e7ee6d64a5adbe49b5934e
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-06-16 20:29:45 +0600

    course-details-page initiated

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardPageComponent } from './page/dashboard-page/dashboard-page.component';
+import { CourseDetailsPageComponent } from './page/course-details-page/course-details-page.component';
 
 
 @NgModule({
   declarations: [
-    DashboardPageComponent
+    DashboardPageComponent,
+    CourseDetailsPageComponent
   ],
   imports: [
     CommonModule,
