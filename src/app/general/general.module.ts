d4a1f0f4b301a2fa6ba74165f89df3f4c38e4504
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-06-22 18:35:29 +0600

    course-preview-page initiated

--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -5,12 +5,14 @@ import { GeneralRoutingModule } from './general-routing.module';
 import { HomePageComponent } from './page/home-page/home-page.component';
 import { ProfilePageComponent } from './page/profile-page/profile-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CoursePreviewPageComponent } from './page/course-preview-page/course-preview-page.component';
 
 @NgModule({
   declarations: [
     CallbackPageComponent,
     HomePageComponent,
-    ProfilePageComponent
+    ProfilePageComponent,
+    CoursePreviewPageComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/general/page/course-preview-page/course-preview-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-preview-page',
+  templateUrl: './course-preview-page.component.html',
+  styleUrl: './course-preview-page.component.scss'
+})
+export class CoursePreviewPageComponent {
+
+}
