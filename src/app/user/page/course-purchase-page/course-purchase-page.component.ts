b954a3a1d1a71b513d5836166d5261d38ff8e02b
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-06-23 12:16:55 +0600

    added course-purchase-page component

---
+++ src/app/user/page/course-purchase-page/course-purchase-page.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../shared/service/auth.service';
+import { BackendApiService } from '../../../shared/service/backend-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { PopNotificationService } from '../../../shared/service/pop-notification.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Observable, map } from 'rxjs';
+
+@Component({
+  selector: 'app-course-purchase-page',
+  templateUrl: './course-purchase-page.component.html',
+  styleUrl: './course-purchase-page.component.scss'
+})
+export class CoursePurchasePageComponent implements OnInit {
+  courseData: any;
+  courseImage: any;
+  paymentOption: any;
+  paymentData: any;
+
+  constructor(
+    private authService: AuthService,
+    private backendApiService: BackendApiService,
+    private route: ActivatedRoute,
+    private popNotificationService: PopNotificationService,
+    private formBuilder: FormBuilder,
+    private sanitizer: DomSanitizer,
+  ) {
+    this.paymentOption = '';
+    this.paymentData = this.formBuilder.group({
+      paymentVendor: ['', Validators.required],
+      senderAccount: ['', Validators.required],
+      amount: [
+        '',
+        [Validators.required, Validators.min(100), Validators.max(100000)],
+      ],
+      trxId: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  fetchCoursePreview(courseId: any): void {
+    this.backendApiService.callGetCoursePreviewAPI(courseId).subscribe({
+      next: (response) => {
+        this.courseData = response.responseBody.coursePreview;
+        this.loadImage(this.courseData.imageUrl);
+      },
+      error: (error) => {
+        this.popNotificationService.error(error.error.errorMessage);
+      },
+    });
+  }
+
+  loadImage(imageUrl: string): void {
+    this.getImage(imageUrl).subscribe({
+      next: (image) => {
+        this.courseImage = this.sanitizer.bypassSecurityTrustUrl(image);
+      },
+      error: (error) => console.error(error),
+    });
+  }
+
+  getImage(imageUrl: string): Observable<string> {
+    return this.backendApiService
+      .callGetContentAPI(imageUrl)
+      .pipe(map((response) => URL.createObjectURL(new Blob([response]))));
+  }
+
+  togglePaymentOption(paymentOption: string) {
+    this.paymentOption = paymentOption;
+    this.paymentData.reset({ paymentVendor: '' });
+  }
+
+  savePaymentInfo(): void {
+    if (this.paymentData.valid) {
+      const paymentInfo = {
+        ...this.paymentData.value,
+        userId: this.authService.getUserId(),
+        courseId: this.courseData.courseId,
+      };
+      this.backendApiService.callSavePaymentAPI(paymentInfo).subscribe({
+        next: (response) => {
+          this.popNotificationService.success(response.responseBody.message);
+          this.paymentData.reset();
+        },
+        error: (error) => {
+          this.popNotificationService.error(error.error.errorMessage);
+        }
+      });
+    }
+  }
+}
