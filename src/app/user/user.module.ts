bc1bb3a46a209bda55cc386f91409cb1cf63acfc
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-06-23 12:15:09 +0600

    added reactiveFormsModule

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { CoursePurchasePageComponent } from './page/course-purchase-page/course-purchase-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { CoursePurchasePageComponent } from './page/course-purchase-page/course-
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
