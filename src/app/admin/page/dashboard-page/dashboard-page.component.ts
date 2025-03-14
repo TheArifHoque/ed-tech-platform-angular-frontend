140c7815f55d003fc142c00da2ff2c867c0c72b6
Ariful Hoque <ariful@brainstation-23.com>
2025-03-14 11:17:50 +0600

    updated admin dashboard

--- src/app/admin/page/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/page/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../shared/service/auth.service';
+import { CommonService } from '../../../shared/service/common.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-page.component.scss'],
 })
 export class DashboardPageComponent {
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private commonService: CommonService
+  ) {}
+
+  getFullname(): string {
+    return this.authService.getFullname();
+  }
+
+  getAdminActionItems(): any[] {
+    return this.commonService.getAdminActionItems();
+  }
 }
